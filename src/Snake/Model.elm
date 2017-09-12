5a6d6c9aab01f88ec2035decbc5836d0e655bf30
Anders Rabo Thorbeck <andtho@knowit.no>
2017-09-12 16:43:08 +0200

    Adds tentative set of Snake.Model types

--- src/Snake/Model.elm
+++ src/Snake/Model.elm
@@ -3,3 +3,80 @@ module Snake.Model exposing (..)
 
 type alias Test =
     String
+
+
+
+-- Model types
+
+
+type alias Model =
+    { gridDims : GridDims
+    , snake : Snake
+    , direction : Direction
+    , food : Food
+    }
+
+
+
+-- From origin (inclusive) to this coord (exclusive)
+
+
+type alias GridDims =
+    Coord
+
+
+type alias Coord =
+    ( Horizontal, Vertical )
+
+
+type alias Horizontal =
+    Int
+
+
+type alias Vertical =
+    Int
+
+
+type alias Snake =
+    List Coord
+
+
+type Direction
+    = Up
+    | Down
+    | Left
+    | Right
+
+
+type alias Food =
+    Coord
+
+
+
+-- Msg types
+
+
+type Msg
+    = ChangeDirection Direction
+
+
+
+-- Helper types
+
+
+type alias Grid =
+    List Row
+
+
+type alias Row =
+    List Tile
+
+
+type Tile
+    = SnakeTile
+    | FoodTile
+    | FreeTile
+
+
+type alias SnakeLength =
+    Int
