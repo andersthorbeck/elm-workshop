820d815ce46b75a8b1fe60597d42326bf5328cc0
Anders Rabo Thorbeck <andtho@knowit.no>
2017-09-12 16:46:07 +0200

    Displays single snake-colored tile

--- src/Snake.elm
+++ src/Snake.elm
@@ -1,13 +1,45 @@
 module Snake exposing (snakeApp)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Snake.Model exposing (..)
 
 
+snakeApp : Html a
 snakeApp =
-    text ("Hello " ++ test)
+    view SnakeTile
 
 
-test : Test
-test =
-    "world"
+view : Tile -> Html a
+view tile =
+    div []
+        [ h1 []
+            [ text "Snake" ]
+        , div []
+            [ viewTile tile ]
+        ]
+
+
+viewTile : Tile -> Html a
+viewTile tile =
+    let
+        color =
+            case tile of
+                SnakeTile ->
+                    "yellow"
+
+                FoodTile ->
+                    "red"
+
+                FreeTile ->
+                    "gray"
+    in
+        div
+            [ style
+                [ ( "width", "10px" )
+                , ( "height", "10px" )
+                , ( "margin", "auto" )
+                , ( "background-color", color )
+                ]
+            ]
+            []
