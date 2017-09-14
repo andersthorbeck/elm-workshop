3471948fd3e07f212e213f4563e5ecc083cec2b5
Anders Rabo Thorbeck <andtho@knowit.no>
2017-09-14 23:30:51 +0200

    Converts a model to a grid

--- src/Snake.elm
+++ src/Snake.elm
@@ -7,22 +7,64 @@ import Snake.Model exposing (..)
 
 snakeApp : Html a
 snakeApp =
-    view testGrid
+    view testModel
 
 
-testGrid : Grid
-testGrid =
-    [ [ SnakeTile, FreeTile ], [ FreeTile, FoodTile ] ]
+testModel : Model
+testModel =
+    { gridDims = ( 3, 3 )
+    , snake = [ ( 1, 1 ), ( 2, 1 ) ]
+    , direction = Left
+    , food = ( 0, 2 )
+    }
 
 
-view : Grid -> Html a
-view grid =
+view : Model -> Html a
+view model =
     div []
         [ h1 []
             [ text "Snake" ]
         , div []
-            [ viewGrid grid ]
+            [ viewGrid (toGrid model) ]
         ]
+
+
+toGrid : Model -> Grid
+toGrid model =
+    let
+        ( numRows, _ ) =
+            model.gridDims
+
+        -- Rows are numbered from the bottom, but rendered from the top.
+        rows =
+            List.reverse (List.range 0 (numRows - 1))
+    in
+        List.map (\r -> toRow model r) rows
+
+
+toRow : Model -> Int -> Row
+toRow model rowNum =
+    let
+        ( _, numColumns ) =
+            model.gridDims
+
+        columns =
+            List.range 0 (numColumns - 1)
+    in
+        List.map
+            -- WTF: Have to reverse row and column here to get correct output??
+            (\c -> toTile model ( c, rowNum ))
+            columns
+
+
+toTile : Model -> Coord -> Tile
+toTile model coord =
+    if List.member coord model.snake then
+        SnakeTile
+    else if coord == model.food then
+        FoodTile
+    else
+        FreeTile
 
 
 viewGrid : Grid -> Html a
